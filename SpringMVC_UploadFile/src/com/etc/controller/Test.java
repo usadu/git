9f3fc17a6f3ed5e042533bfe43a60b6a54de5f3e
zq <893747750@qq.com>
2019-01-02 14:13:39 +0800

    Signed-off-by: zq <893747750@qq.com>

--- SpringMVC_UploadFile/src/com/etc/controller/Test.java
+++ SpringMVC_UploadFile/src/com/etc/controller/Test.java
@@ -1,10 +1,7 @@
 package com.etc.controller;
 
-<<<<<<< HEAD
+
 public class Test {
 			private int a;
-=======
-public class Tesd {
 
->>>>>>> branch 'master' of https://github.com/usadu/git.git
 }
